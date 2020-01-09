3f78933ddf5030b6e277f103a147bc9e1996ff80
wootguy <w00tguy123@gmail.com>
2020-01-09 23:06:19 +0000

    Moved all ClassicModeDeluxe resources into a single folder
    Apparently map scripts can load from the plugins folder.
    
    The models/sounds/sprites were moved in preparation for converting this to a separate repo, since it's complicated to work with multiple repos in the same folder (svencoop_addon in this case). The install.bat file will move everything into the right place.

---
+++ scripts/plugins/ClassicModeDeluxe/install.bat
@@ -0,0 +1,26 @@
+@echo off
+cls
+
+echo WARNING: Existing models/sounds/sprites/cfgs will be overwritten!
+echo.
+
+pause
+echo.
+
+echo Copying models...
+xcopy /i/e/y/q models ..\..\..\models
+echo.
+
+echo Copying sounds...
+xcopy /i/e/y/q sound ..\..\..\sound
+echo.
+
+echo Copying sprites...
+xcopy /i/e/y/q sprites ..\..\..\sprites
+echo.
+
+echo Copying default_map_settings.cfg ...
+copy default_map_settings.cfg ..\..\..\default_map_settings.cfg
+echo.
+
+pause
